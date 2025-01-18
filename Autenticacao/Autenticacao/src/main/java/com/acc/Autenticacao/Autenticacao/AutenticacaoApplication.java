e22a347b22a62c9cfc03f51fcce4d116775d9df0
Gabriel Nobrega <140744513+nobregas@users.noreply.github.com>
2025-01-18 10:24:10 -0300

    descompactacao dos .zip

---
+++ Autenticacao/Autenticacao/src/main/java/com/acc/Autenticacao/Autenticacao/AutenticacaoApplication.java
@@ -0,0 +1,13 @@
+package com.acc.Autenticacao.Autenticacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacaoApplication.class, args);
+	}
+
+}
