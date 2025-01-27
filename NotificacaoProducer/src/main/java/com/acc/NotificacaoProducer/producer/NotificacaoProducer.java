6c0d0f1a55df819854af0df9d571e8223b3c62c1
Gabriel Nobrega <140744513+nobregas@users.noreply.github.com>
2025-01-27 14:47:43 -0300

    implementacao base de um futuro mecanismo de fallback de notificacoes

--- NotificacaoProducer/src/main/java/com/acc/NotificacaoProducer/producer/NotificacaoProducer.java
+++ NotificacaoProducer/src/main/java/com/acc/NotificacaoProducer/producer/NotificacaoProducer.java
@@ -4,6 +4,9 @@ package com.acc.NotificacaoProducer.producer;
 import com.acc.NotificacaoProducer.models.NotificacaoPayload;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +19,19 @@ public class NotificacaoProducer {
         this.rabbitTemplate.setMessageConverter(messageConverter);
     }
 
+    @Retryable(
+            value = { Exception.class },
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000, multiplier = 2)
+    )
     public void sendToQueue(NotificacaoPayload payload) {
         rabbitTemplate.convertAndSend("notificacao.exchange", "notificacao.routingkey", payload);
+    }
+
+    @Recover
+    public void fallbackSendToQueue(Exception e, NotificacaoPayload payload) {
+        System.err.println("Falha ao enviar mensagem após múltiplas tentativas: " + e.getMessage());
+
+        // Aqui poderia ficar armazenado em um banco como redis
     }
 }
